2c94dade25bb85296f5cc3eefc9728ecc6f03929
Harsha <143327122+Appr0ximity@users.noreply.github.com>
2026-05-19 21:00:00 +0530

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.cpp
@@ -0,0 +1,26 @@
+#include <cstring> 
+class MyHashSet {
+private:
+    int set[31251];
+
+    int getMask(int key) {
+        return 1u << (key % 32);
+    }
+
+public:
+    MyHashSet() {
+        memset(set, 0, sizeof(set));
+    }
+
+    void add(int key) {
+        set[key / 32] |= getMask(key);
+    }
+
+    void remove(int key) {
+        set[key / 32] &= ~getMask(key);
+    }
+
+    bool contains(int key) {
+        return (set[key / 32] & getMask(key)) != 0;
+    }
+};
