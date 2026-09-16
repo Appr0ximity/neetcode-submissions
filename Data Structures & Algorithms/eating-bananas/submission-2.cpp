aefcb7569eef2e73483dde9f261d8f9b4c6447fb
Harsha <143327122+Appr0ximity@users.noreply.github.com>
2026-09-16 23:25:53 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxL = *max_element(piles.begin(), piles.end());
+        if(h == piles.size()) return maxL;
+        int i = 1, j = maxL, mid;
+        while(i<=j){
+            mid = i+(j-i)/2;
+            if(countHours(mid, piles) > h){
+                i = mid + 1;
+            }else if(countHours(mid, piles) <= h && countHours(mid - 1, piles) <= h){
+                j = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return i;
+    }
+    int countHours(int k, vector<int> &piles){
+        if(k == 0) return INT_MAX;
+        int hours = 0;
+        for(int i =0; i<piles.size(); i++){
+            hours += ceil((double)piles[i]/k);
+        }
+        return hours;
+    }
+};
